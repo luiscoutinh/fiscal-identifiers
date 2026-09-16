4a9a6eb096a534ca03a75b9fad0bab04adf66438
Luis Coutinho <luis@luiscoutinho.pt>
2026-09-16 07:56:58 +0100

    refactor: use extensible identifier type value object

--- src/FiscalIdentifierValidator.php
+++ src/FiscalIdentifierValidator.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace FiscalIdentifiers;
 
-use FiscalIdentifiers\Enums\IdentifierType;
 use FiscalIdentifiers\Enums\ValidationDecision;
 use FiscalIdentifiers\Enums\ValidationStatus;
 use FiscalIdentifiers\Registry\CountryRegistry;
