7234273c082d49499eb9b03a417bd59c6f57fae7
Luis Coutinho <luis@luiscoutinho.pt>
2026-09-16 07:55:30 +0100

    refactor: name Portuguese normalizer after NIF semantics

---
+++ src/Countries/PT/PortugalNifNormalizer.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FiscalIdentifiers\Countries\PT;
+
+use FiscalIdentifiers\Contracts\Normalizer;
+
+final class PortugalNifNormalizer implements Normalizer
+{
+    public function normalize(string $value): string
+    {
+        $value = strtoupper(trim($value));
+        $value = preg_replace('/[\s\-.]+/', '', $value) ?? $value;
+
+        return str_starts_with($value, 'PT') ? substr($value, 2) : $value;
+    }
+}
